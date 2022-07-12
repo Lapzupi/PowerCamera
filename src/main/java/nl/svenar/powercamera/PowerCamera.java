17520466b913a1b8a458b752954baa41ec35576e
Omer Oreg <sarhatabaot1@gmail.com>
2022-07-12 10:07:25 +0300

    Added managers

--- src/main/java/nl/svenar/powercamera/PowerCamera.java
+++ src/main/java/nl/svenar/powercamera/PowerCamera.java
@@ -5,8 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import co.aikar.commands.PaperCommandManager;
+import com.github.sarhatabaot.kraken.core.db.ConnectionFactory;
 import nl.svenar.powercamera.commands.PowerCameraCommand;
-import nl.svenar.powercamera.config.CameraStorage;
+import nl.svenar.powercamera.storage.configurate.CameraConfigurate;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -15,47 +16,86 @@ import org.bukkit.plugin.java.JavaPlugin;
 import nl.svenar.powercamera.config.PluginConfig;
 import nl.svenar.powercamera.listeners.OnJoin;
 import nl.svenar.powercamera.listeners.OnMove;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.configurate.ConfigurateException;
 
 public class PowerCamera extends JavaPlugin {
     private final String pluginChatPrefix = ChatColor.BLACK + "[" + ChatColor.AQUA + getDescription().getName() + ChatColor.BLACK + "] ";
 
     private PluginConfig pluginConfig;
-    private CameraStorage cameraStorage;
+    private CameraConfigurate cameraConfigurate;
 
     //These Should be in managers/caches
     private PlayerManager playerManager;
+    private CameraManager cameraManager;
+
+    private ConnectionFactory<PowerCamera> connectionFactory;
 
     private final Instant startTime = Instant.now();
 
     @Override
     public void onEnable() {
+        initConfig();
         try {
-            this.pluginConfig = new PluginConfig(this);
-            this.cameraStorage = new CameraStorage(this);
+            this.cameraConfigurate = new CameraConfigurate(this);
         } catch (ConfigurateException e) {
-            getLogger().severe(() -> "Could not initialize cameras.conf or config.conf");
+            getLogger().severe(() -> "Could not initialize cameras.conf");
             getLogger().warning(() -> "Please fix any errors and reload the plugin.");
             e.printStackTrace();
         }
 
-        this.playerManager = new PlayerManager();
-
+        initManagers();
         Bukkit.getServer().getPluginManager().registerEvents(new OnMove(this), this);
         Bukkit.getServer().getPluginManager().registerEvents(new OnJoin(this), this);
 
+        initCommands();
+
+        getLogger().info(() -> "Enabled %s v%s".formatted(getDescription().getName(), getDescription().getVersion()));
+
+        @SuppressWarnings("unused")
+        Metrics metrics = new Metrics(this, 9107);
+    }
+
+    private void initConfig() {
+        try {
+            this.pluginConfig = new PluginConfig(this);
+        } catch (ConfigurateException e) {
+            getLogger().severe(() -> "Could not initialize config.conf");
+            getLogger().warning(() -> "Please fix any errors and reload the plugin.");
+            e.printStackTrace();
+        }
+    }
+
+    private void initCommands() {
         PaperCommandManager commandManager = new PaperCommandManager(this);
         commandManager.enableUnstableAPI("help");
         commandManager.enableUnstableAPI("brigadier");
         commandManager.registerCommand(new PowerCameraCommand(this));
         commandManager.getCommandCompletions().registerCompletion("points", context ->
-                generateIntsInRange(getCameraStorage().getCamera(playerManager.getSelectedCameraId(context.getPlayer().getUniqueId())).getPoints().size()).stream().map(Object::toString).toList()
+                generateIntsInRange(cameraManager.getCamera(playerManager.getSelectedCameraId(context.getPlayer().getUniqueId())).getPoints().size()).stream().map(Object::toString).toList()
         );
-        commandManager.getCommandCompletions().registerCompletion("cameras", context -> getCameraStorage().getCameraIds());
+        commandManager.getCommandCompletions().registerCompletion("cameras", context -> cameraManager.getCameraIds());
+    }
 
-        getLogger().info(() -> "Enabled %s v%s".formatted(getDescription().getName(), getDescription().getVersion()));
-        @SuppressWarnings("unused")
-        Metrics metrics = new Metrics(this, 9107);
+    private void initManagers() {
+        this.playerManager = new PlayerManager();
+        this.cameraManager = new CameraManager(this);
+    }
+
+    private void initStorage() {
+        String databaseType = pluginConfig.getDatabase().getType();
+        switch (databaseType){
+            case "hocon": {
+
+            }
+            case "sql": {
+
+            }
+            case "sqlite": {
+
+            }
+        }
+
     }
 
     @Override
@@ -77,8 +117,8 @@ public class PowerCamera extends JavaPlugin {
         return startTime;
     }
 
-    public CameraStorage getCameraStorage() {
-        return cameraStorage;
+    public CameraConfigurate getCameraStorage() {
+        return cameraConfigurate;
     }
 
     public PlayerManager getPlayerManager() {
@@ -86,7 +126,7 @@ public class PowerCamera extends JavaPlugin {
     }
 
 
-    private List<Integer> generateIntsInRange(int max) {
+    private @NotNull List<Integer> generateIntsInRange(int max) {
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < max; i++){
 			list.add(i);
