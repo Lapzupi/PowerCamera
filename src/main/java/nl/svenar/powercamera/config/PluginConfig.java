e0e7ba78b5c6e2484f5052e85273027e66a39529
Omer Oreg <sarhatabaot1@gmail.com>
2022-07-12 10:07:17 +0300

    Added database

--- src/main/java/nl/svenar/powercamera/config/PluginConfig.java
+++ src/main/java/nl/svenar/powercamera/config/PluginConfig.java
@@ -6,6 +6,7 @@ import com.github.sarhatabaot.kraken.core.config.HoconConfigurateFile;
 import com.github.sarhatabaot.kraken.core.config.Transformation;
 import nl.svenar.powercamera.PowerCamera;
 import org.jetbrains.annotations.NotNull;
+import org.spongepowered.configurate.CommentedConfigurationNode;
 import org.spongepowered.configurate.ConfigurateException;
 
 public class PluginConfig extends HoconConfigurateFile<PowerCamera> {
@@ -13,6 +14,8 @@ public class PluginConfig extends HoconConfigurateFile<PowerCamera> {
 	private int singleFrameDuration;
 	private Join join;
 	private CameraEffects cameraEffects;
+
+	private Database database;
 
 	public static class Join {
 		private boolean showOnce;
@@ -32,6 +35,64 @@ public class PluginConfig extends HoconConfigurateFile<PowerCamera> {
 
 		public void setCameraId(final String cameraId) {
 			this.cameraId = cameraId;
+		}
+	}
+
+	public static class Database {
+		private String type;
+		private String address;
+		private int port;
+		private String databaseName;
+
+		private String username;
+		private String password;
+
+		public void setType(final String type) {
+			this.type = type;
+		}
+
+		public void setAddress(final String address) {
+			this.address = address;
+		}
+
+		public void setPort(final int port) {
+			this.port = port;
+		}
+
+		public void setDatabaseName(final String databaseName) {
+			this.databaseName = databaseName;
+		}
+
+		public void setUsername(final String username) {
+			this.username = username;
+		}
+
+		public void setPassword(final String password) {
+			this.password = password;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+		public String getAddress() {
+			return address;
+		}
+
+		public int getPort() {
+			return port;
+		}
+
+		public String getDatabaseName() {
+			return databaseName;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public String getPassword() {
+			return password;
 		}
 	}
 
@@ -76,6 +137,15 @@ public class PluginConfig extends HoconConfigurateFile<PowerCamera> {
 		this.cameraEffects = new CameraEffects();
 		this.cameraEffects.setSpectator(rootNode.node("camera-effects").node("spectator").getBoolean());
 		this.cameraEffects.setInvisible(rootNode.node("camera-effects").node("invisible").getBoolean());
+
+		this.database = new Database();
+		final CommentedConfigurationNode databaseNode = rootNode.node("database");
+		this.database.setDatabaseName(databaseNode.node("database").getString());
+		this.database.setAddress(databaseNode.node("address").getString());
+		this.database.setPort(databaseNode.node("port").getInt());
+		this.database.setType(databaseNode.node("type").getString());
+		this.database.setUsername(databaseNode.node("username").getString());
+		this.database.setPassword(databaseNode.node("password").getString());
 	}
 
 	@Override
@@ -94,6 +164,10 @@ public class PluginConfig extends HoconConfigurateFile<PowerCamera> {
 
 	public CameraEffects getCameraEffects() {
 		return cameraEffects;
+	}
+
+	public Database getDatabase() {
+		return database;
 	}
 
 	public int getSingleFrameDuration() {
